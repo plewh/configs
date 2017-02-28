8dd10c989043eecfe7e676b707e6d895e652499b
plewh <ashhep@gmail.com>
2017-02-28 19:08:36 +0930

    added tmux config

--- install_script.sh
+++ install_script.sh
@@ -2,12 +2,13 @@
 #    install_script
 #        - run me after a fresh install to add custom settings
 #        - things:
-#            - install git
-#            - install vim
+#            - install and setup  git
+#            - install vim, setup vimrc
 #            - install c++ compiler
 #            - install tmux
 #            - install SDL2
 #            - install SDL2 ttf library
+#            - install tag file generator
 #            - download and apply cool DooM wallpaper from github
 #            - download and install mouse_accel script 
 #            - unattended-upgrades to silently do upgrades without nagging
@@ -21,29 +22,35 @@ echo "********** do apt-gets                    **********"
 sudo apt -y update && sudo apt -y dist-upgrade
 echo " "
 echo "********** installing git                 **********"
-sudo apt-get -y install git
+sudo apt -y install git
 git config --global user.email "ashhep@gmail.com"
 git config --global user.name "plewh"
 git config --global push.default simple
 git config --global core.editor "vim"
 echo " "
 echo "********** installing vim                 **********"
-sudo apt-get -y install vim
+sudo apt -y install vim
 echo " "
 echo "********** installing vimrc               **********"
 wget -O /home/plewh/.vimrc https://github.com/plewh/configs/raw/master/.vimrc
 echo " "
 echo "********** installing c++ compiler        **********"
-sudo apt-get -y install g++
+sudo apt -y install g++
 echo " "
 echo "********** installing tmux                **********"
-sudo apt-get -y install tmux
+sudo apt -y install tmux
+echo " "
+echo "********** installing tmux.conf           **********"
+wget -O /home/plewh/.tmux.conf https://github.com/plewh/configs/raw/master/tmux.conf
 echo " "
 echo "********** installing SDL2                **********"
-sudo apt-get -y install libsdl2-2.0-0
-sudo apt-get -y install libsdl2-dev
-sudo apt-get -y install libsdl2-ttf-2.0-0
-sudo apt-get -y install libsdl2-ttf-dev
+sudo apt -y install libsdl2-2.0-0
+sudo apt -y install libsdl2-dev
+sudo apt -y install libsdl2-ttf-2.0-0
+sudo apt -y install libsdl2-ttf-dev
+echo " "
+echo "********** installing exuberant  ctags    **********"
+sudo apt -y install exuberant-ctags
 echo " "
 echo "********** installing cool DooM wallpaper **********"
 wget -O /home/plewh/Downloads/wpaper.png https://github.com/plewh/configs/raw/master/d.png
@@ -54,7 +61,7 @@ echo "********** installing mouse accel script  **********"
 wget -O /etc/profile.d/mouse_accel.sh https://github.com/plewh/configs/raw/master/mouse_accel.sh
 echo " "
 echo "********** installing unattended-upgrades **********"
-sudo apt-get -y install unattended-upgrades
+sudo apt -y install unattended-upgrades
 sudo dpkg-reconfigure -plow unattended-upgrades
 echo " "
 echo " "
